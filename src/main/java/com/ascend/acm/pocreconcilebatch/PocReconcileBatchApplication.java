9d5e5d112b321f1fa1cc885cac4646bd6e19000f
Sirimongkol Panwa <sirimongkol.pan@ascendcorp.com>
2017-10-25 11:10:21 +0700

    Init project

---
+++ src/main/java/com/ascend/acm/pocreconcilebatch/PocReconcileBatchApplication.java
@@ -0,0 +1,12 @@
+package com.ascend.acm.pocreconcilebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocReconcileBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocReconcileBatchApplication.class, args);
+	}
+}
